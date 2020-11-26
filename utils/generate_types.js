ec0ae43497d6755656f168a54a9155e14d6190dd
Zane Starr <zcstarr@gmail.com>
2020-11-25 17:21:07 -0800

    feat: add support for generating package config types

--- utils/generate_types.js
+++ utils/generate_types.js
@@ -1,19 +1,23 @@
 const JsonSchemaTranspiler = require("@json-schema-tools/transpiler").default;
-const mySchema = require("../src/machine-config-schema.json")
+const machineConfigSchema = require("../src/machine-config-schema.json")
+const machinePackageSchema = require("../src/machine-config-package-schema.json")
 const Dereferencer = require("@json-schema-tools/dereferencer").default;
 const fs = require('fs-extra')
 
-async function generate(){
-const dereffer = new Dereferencer(mySchema);
-const schema = await dereffer.resolve()
-const transpiler = new JsonSchemaTranspiler(schema);
-fs.writeFile("./src/generated/machine_config_schema.ts", transpiler.toTypescript());
+async function generate(fileName, schema) {
+    const dereffer = new Dereferencer(schema);
+    const dereffedSchema = await dereffer.resolve()
+    const transpiler = new JsonSchemaTranspiler(dereffedSchema);
+    fs.writeFile(fileName, transpiler.toTypescript());
 }
-try {
-generate()
-}catch(e){
-    console.log(e)
-}
+(async function () {
+    try {
+        await generate("./src/generated/machine_config_schema.ts", machineConfigSchema)
+        await generate("./src/generated/machine_config_pkg_schema.ts", machinePackageSchema)
+    } catch (e) {
+        console.log(e)
+    }
+})()
 
 /*console.log(transpiler.toRust());
 console.log(transpiler.to("go")); // same thing, different form/interface
