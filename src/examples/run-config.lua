5079ee73d2f6baa1449524c64f69192fb1dec1a0
Zane Starr <zcstarr@gmail.com>
2020-11-30 11:45:51 -0800

    feat: add example support that demonstrates end 2 end functionality

---
+++ src/examples/run-config.lua
@@ -0,0 +1,8 @@
+-- Load the Cartesi module
+local cartesi = require"cartesi"
+-- Instantiate machine from configuration
+local machine = cartesi.machine(require(arg[1]))
+-- Run machine until it halts
+while not machine:read_iflags_H() do
+    machine:run(math.maxinteger)
+end
