0740fcb20e9d646dd9d3c10486eab6fd259108f3
Zane Starr <zcstarr@gmail.com>
2020-12-08 16:21:54 -0800

    feat: add support for bundling and installing bundles

---
+++ src/packager/bundle.test.ts
@@ -0,0 +1,59 @@
+import * as bundle from "./bundle"
+import fs from 'fs-extra';
+import {Storage} from "../storage"
+import {readableByteStreamToBuffer} from "../encoders"
+import {expect} from "chai";
+import { MemoryProvider } from "../storage/memory";
+import path from "path"
+import os from "os"
+import _ from "lodash"
+import rimraf from "rimraf"
+import util from "util"
+import { CID } from "multiformats";
+import { install } from ".";
+const rmAll = util.promisify(rimraf);
+
+
+
+//TODO fix test to be more generic
+describe("bundle assets test", ()=>{
+
+    const testPath = __dirname + "/../fixtures/parser/cartesi/images/linux.bin"
+    const testAsset = fs.readFileSync(testPath)
+    //TODO needs better test to check correct pkg results 
+
+    it.only("should bundle and install asset into a carti compatible format",async ()=>{
+        const bundleProvider = new MemoryProvider()
+        const bundleStorage = new Storage(bundleProvider)
+        const remoteProvider = new MemoryProvider()
+        const remoteStorage = new Storage(remoteProvider)
+        const bundleMeta: bundle.BundleMeta = {
+            bundleType: "ram",
+            name: "testBundle",
+            version: "1.0.0",
+            path: testPath,
+        } 
+        const bun = await bundle.bundle(bundleMeta, bundleStorage)         
+        console.log(bun)
+        const cid = CID.parse(bun.id)
+        if(!cid){
+            throw "invalid cid"
+        }
+        const data = await bundleStorage.get(cid)
+        const bundledData = await readableByteStreamToBuffer(data)
+        let testBundle:any = Object.assign({},bundleMeta,{path: undefined, deps:undefined})
+        testBundle.id = cid.toString();
+        testBundle.fileName = "linux.bin"
+        delete testBundle.path
+        expect(JSON.stringify(bundleProvider.metaStorage[testBundle.Id]),JSON.stringify(bun))
+        
+        expect(_.isEqual(bundledData, testAsset)).true
+        expect(_.isEqual(testBundle, bun)).true
+
+        await bundle.install(bun, bundleStorage, remoteStorage) 
+        const installedBundle = await remoteStorage.get(CID.parse(bun.id))
+        expect(_.isEqual(await readableByteStreamToBuffer(installedBundle), bundledData)).true
+        expect(JSON.stringify(remoteProvider.metaStorage[testBundle.Id]),JSON.stringify(bun))
+        
+    })
+})
---
+++ src/packager/bundle.ts
@@ -0,0 +1,45 @@
+import { fstat } from "fs";
+import { Bundle, Deps } from "../generated/bundle_config_schema";
+import fs from 'fs-extra'
+import path from 'path'
+import {Storage} from "../storage"
+import {binMemEncoder} from "./package_encoders"
+import { Fetcher } from "../fetcher";
+import { CID } from "multiformats"
+import {readableByteStreamToBuffer} from "../encoders";
+// bundle takes source path of asset that you'd like to package stores it in 
+// default location of ./.bundles/ . and uses disktorage interface to ensure
+// consistent representation of assets depending on the downstream use case
+
+type BundleType = "ram" | "rom" | "raw"
+
+export interface BundleMeta {
+    name: string
+    version: string
+    bundleType: BundleType 
+    deps?: Deps
+    path: string
+}
+
+export async function bundle(config: BundleMeta, storage:Storage): Promise<Bundle> {
+    
+   const file = await fs.readFile(config.path)
+   const cid = await storage.put(file, binMemEncoder)
+   const fileName = path.basename(config.path)
+   return {
+       bundleType: config.bundleType,
+       name: config.name,
+       fileName,
+       id : cid.toString(),
+       deps:config.deps,
+       version: config.version,
+   }
+}
+//TODO eventually install will need to take the cid from the bundle and either 
+// liek for like put with cid specification where the raw data 
+export async function install(bundle: Bundle, fetcher: Fetcher, storage: Storage) {
+    const cid = CID.parse(bundle.id)
+    const byteStream = await fetcher.get(cid)
+    const content = await readableByteStreamToBuffer(byteStream)
+    return storage.provider.put(cid, content, bundle);
+}
