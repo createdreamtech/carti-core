71bd27b060ffd70d629c4e7797864d2734557070
Zane Starr <zcstarr@gmail.com>
2021-01-03 13:47:06 -0800

    chore: lint and comment

--- src/encoders/hashers.ts
+++ src/encoders/hashers.ts
@@ -1,12 +1,12 @@
 import { hasher } from "multiformats"
-import sha3, {Message} from 'js-sha3'
+import sha3, { Message } from 'js-sha3'
+type EncodingFn = (message: Message) => number[]
+const encoder = (enc: EncodingFn) => (bb: Message) => new Uint8Array(enc(bb))
 
-type EncodingFn =( message: Message) => number[]
-const encoder = (enc: EncodingFn) => (bb:Message) => new Uint8Array(enc(bb))
-
-export const keccak256 = hasher.from({ 
-        code: 0x1b, 
-        name: 'keccak-256', 
-        encode: encoder(sha3.keccak256.array) 
+// keccak256 defines hashing function used to make cid's from data 
+export const keccak256 = hasher.from({
+        code: 0x1b,
+        name: 'keccak-256',
+        encode: encoder(sha3.keccak256.array)
 });
 
