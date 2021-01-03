7cb24b92e4cbfa60bea83105f001809bb5812d56
Zane Starr <zcstarr@gmail.com>
2021-01-03 13:37:42 -0800

    feat: add support for partial machine configuration

--- src/parser/index.ts
+++ src/parser/index.ts
@@ -1,3 +1,3 @@
-import { parse } from "path";
 export { generateLuaConfig } from "./lua_config_template"
 export { parseLuaMachineConfig } from "./lua_parser"
+export {parseBundlesFile, writeBundlesFile} from "./bundle_parser"
