d42a8e5248187ded87627c53f800a375218b88bb
Zane Starr <zcstarr@gmail.com>
2021-01-27 14:50:19 -0800

    fix: correct following redirects properly

--- src/fetcher/index.ts
+++ src/fetcher/index.ts
@@ -1,6 +1,6 @@
 import { CID } from "multiformats"
 import { Readable } from "stream"
-import https from "https"
+import {https} from "follow-redirects"
 import util from "util"
 export type { Fetcher } from "./fetcher"
 import { Fetcher } from "./fetcher"
@@ -10,9 +10,12 @@ const promisify = util.promisify
 export function makeHttpFetcher(uri: string): Fetcher {
     return {
         get: (cid: CID): Promise<Readable> => {
-            return new Promise((resolve) => {
-                https.get(uri, (message) => {
-                    resolve(message)
+            return new Promise((resolve, reject) => {
+                https.get(uri, (response) => {
+                    if(!response || response.statusCode! >=400){
+                        reject(new Error(`error: couldn't retrieve data from ${uri} code:${response.statusCode}`))
+                    }
+                    resolve(response)
                 })
             })
         }
